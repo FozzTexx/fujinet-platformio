c0966da34fc23e0ced65faf0fa5182b1c12da783
Chris Osborn <fozztexx@fozztexx.com>
2025-07-24 14:56:22 -0700

    Adding improperly formatted cpp file to test PR check

---
+++ badly-formatted.cpp
@@ -0,0 +1,18 @@
+/*******************
+ * Formatting test
+ * DO NOT MERGE
+ *******************/
+
+#include <stdio.h>
+
+class MyClass
+{
+public:
+  void test();
+};
+
+MyClass::test()
+{
+  printf("Testing\n");
+  return;
+}
