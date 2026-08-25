dc326792b6dc5ee233801751beccbfa0d412fb78
Chris Osborn <fozztexx@fozztexx.com>
2026-08-25 12:18:06 -0700

    [c64] Move bus handling out of iecFuji and into virtualDevice
    Move all the bus handling out of iecFuji and put it some place where
    it doesn't need to be replicated into every single device. It really
    belongs in the bus itself, but this will work until ready to deal with
    that refactor.

--- lib/bus/iec/virtualDevice.h
+++ lib/bus/iec/virtualDevice.h
@@ -3,20 +3,33 @@
 
 #ifdef BUILD_IEC
 
-#include "IECFileDevice.h"
+#include "IECDevice.h"
 #include "FujiIECPacket.h"
 
-class virtualDevice : public IECFileDevice
+#include <memory>
+
+// FIXME - belongs in IECBusHandler
+typedef enum
+{
+    DEVICE_ERROR = -1,
+    DEVICE_IDLE = 0,      // Ready and waiting
+    DEVICE_ACTIVE = 1,
+    DEVICE_LISTEN = 2,    // A command is recieved and data is coming to us
+    DEVICE_TALK = 3,      // A command is recieved and we must talk now
+    DEVICE_PAUSED = 4,    // Execute device command
+} device_state_t;
+
+class virtualDevice : public IECDevice
 {
 protected:
-    device_state_t state;
-
     virtual void shutdown() {};
 
     virtual bool processCommand(const FujiIECPacket &packet) = 0;
 
-#ifdef UNUSED
     // FIXME - this should all be handled by the bus, not individual devices
+    ByteBuffer _payload;
+    std::unique_ptr<FujiIECPacket> _activePacket;
+    device_state_t _state;
     void talk(uint8_t secondary) override;
     void listen(uint8_t secondary) override;
     void untalk() override;
@@ -26,17 +39,19 @@ protected:
     void write(uint8_t data, bool eoi) override;
     uint8_t read() override;
     void task() override;
-#endif /* UNUSED */
+    void process_cmd();
 
+#ifdef UNUSED
     // IEC channel methods
     bool open(uint8_t channel, const char *name) override;
     void close(uint8_t channel) override;
     uint8_t read(uint8_t channel, uint8_t *buffer, uint8_t bufferSize, bool *eoi) override;
     uint8_t write(uint8_t channel, uint8_t *buffer, uint8_t bufferSize, bool eoi) override;
+#endif /* UNUSED */
 
  public:
-    virtualDevice();
-    virtualDevice(uint8_t devnr) : IECFileDevice(devnr) {}
+    virtualDevice() = default;
+    virtualDevice(uint8_t devnr) : IECDevice(devnr) {}
 };
 
 #endif /* BUILD_IEC */
