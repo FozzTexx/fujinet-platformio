70b9f627d4b6a96c5aa334971cfefa4d17b6ee7c
Chris Osborn <fozztexx@fozztexx.com>
2026-09-01 17:50:20 -0700

    [apple2] get disk ID from bus

--- lib/device/fujiDevice/fujiDevice.h
+++ lib/device/fujiDevice/fujiDevice.h
@@ -224,7 +224,13 @@ public:
 
     fujiDisk *get_disk(int i) { return &_fnDisks[i]; }
     virtual DISK_DEVICE *get_disk_dev(int i) { return &_fnDisks[i].disk_dev; }
-    fujiDeviceID_t get_disk_id(int drive_slot) { return _fnDisks[drive_slot].disk_dev.id(); }
+    fujiDeviceID_t get_disk_id(int drive_slot) {
+#if 0 // COCO
+        return _fnDisks[drive_slot].disk_dev.id();
+#else
+        return SYSTEM_BUS.fujiIDForDevice(&_fnDisks[drive_slot].disk_dev);
+#endif
+    }
 
     void populate_slots_from_config();
     void populate_config_from_slots();
