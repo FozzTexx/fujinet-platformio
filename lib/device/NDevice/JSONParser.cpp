051899fef88af7ca63ddd9356ef9a5d0e1158a9e
Chris Osborn <fozztexx@fozztexx.com>
2026-09-05 13:47:45 -0700

    Get rid of JSONParser::_remaining

--- lib/device/NDevice/JSONParser.cpp
+++ lib/device/NDevice/JSONParser.cpp
@@ -1,21 +1,8 @@
 #include "JSONParser.h"
 
-fujiError_t JSONParser::read(std::string &buffer, size_t length)
-{
-    fujiError_t err = Parser::read(buffer, std::min<size_t>(_remaining, length));
-    if (err == FUJI_ERROR::NONE)
-        _remaining -= length;
-    return err;
-}
-
 fujiError_t JSONParser::write(std::string &buffer)
 {
     return FUJI_ERROR::UNSPECIFIED;
-}
-
-size_t JSONParser::available()
-{
-    return _remaining;
 }
 
 off_t JSONParser::seek(off_t offset, int whence)
@@ -28,12 +15,10 @@ error_is_true JSONParser::setQuery(const std::string &query)
     std::string buffer;
 
     _json.setReadQuery(query, 0);
-    _remaining = _json.available();
-    buffer.resize(_remaining);
-    _json.readValue(reinterpret_cast<uint8_t *>(buffer.data()), _remaining);
+    buffer.resize(_json.available());
+    _json.readValue(reinterpret_cast<uint8_t *>(buffer.data()), buffer.size());
     buffer.resize(strlen(buffer.c_str()));
     buffer = SYSTEM_BUS.unicodeTextToNative(buffer);
-    _remaining = buffer.size();
     *_protocol->receiveBuffer += buffer;
     RETURN_SUCCESS_AS_FALSE();
 }
@@ -42,13 +27,4 @@ error_is_true JSONParser::parse()
 {
     _json.parse();
     RETURN_SUCCESS_AS_FALSE();
-}
-
-NetworkStatus JSONParser::status()
-{
-    NetworkStatus ns;
-
-    ns.connected = _remaining > 0;
-    ns.error = _remaining > 0 ? NDEV_STATUS::SUCCESS : NDEV_STATUS::END_OF_FILE;
-    return ns;
 }
--- lib/device/NDevice/JSONParser.h
+++ lib/device/NDevice/JSONParser.h
@@ -8,7 +8,9 @@ class JSONParser : public Parser
 {
 protected:
     FNJSON _json;
+#ifdef OBSOLETE
     unsigned short _remaining = 0;
+#endif /* OBSOLETE */
 
 public:
     using Parser::Parser;
@@ -17,15 +19,21 @@ public:
         _json.setProtocol(_protocol);
     }
 
+#ifdef OBSOLETE
     fujiError_t read(std::string &buffer, size_t length) override;
+#endif /* OBSOLETE */
     fujiError_t write(std::string &buffer) override;
+#ifdef OBSOLETE
     size_t available() override;
+#endif /* OBSOLETE */
     off_t seek(off_t offset, int whence) override;
 
     error_is_true setQuery(const std::string &query) override;
     error_is_true parse() override;
 
+#ifdef OBSOLETE
     NetworkStatus status() override;
+#endif /* OBSOLETE */
 };
 
 #endif /* JSONPARSER_H */
